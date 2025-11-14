2d36bfd70c86db71b429d945b78e40bc4cb6c614
Dennis King Tran <dennis.tran@hotmail.de>
2025-11-14 20:45:06 +0100

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent {
+
+}
